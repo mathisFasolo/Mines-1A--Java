0dbd60f089a9db27fd08566493def477490bc485
mathisFasolo <mathis.fasolo@icloud.com>
2018-01-16 15:15:52 +0100

    First commit fabriquePoint

---
+++ FabriquePoint.java
@@ -0,0 +1,23 @@
+package TD1;
+import java.lang.Exception;
+
+public abstract class FabriquePoint
+{
+    public static InterPoint create (String typePoint, double arg1, double arg2) throws Exception
+    {
+        if(typePoint == "Point")
+        {
+            Point p1 = new Point(arg1, arg2);
+            return p1;
+        }
+        else if (typePoint == "Point2")
+        {
+            Point2 p2 = new Point2(arg1,arg2);
+            return p2;
+        }
+        else
+        {
+            throw new Exception("Mauvais argument dans la fabrique a point");
+        }
+    }
+}
