e5861ff415db4afc70e25eac5fd5a495c9a957a3
mathisFasolo <mathis.fasolo@icloud.com>
2018-01-16 14:48:52 +0100

    First commit interface

---
+++ InterPoint.java
@@ -0,0 +1,16 @@
+package TD1;
+
+public interface InterPoint
+{
+    /**
+     * Permet d'obtenir la position x du point dans le plan
+     * @return float
+     */
+    float getX();
+
+    /**
+     * Permet d'obtenir la position y du point dans le plan
+     * @return float
+     */
+    float getY();
+}
